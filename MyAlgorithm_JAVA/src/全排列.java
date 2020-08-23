5df76818f2eac6eb6a7873fc7de3c0a9dda8e292
ranj <ranjin@quantdo.com.cn>
2020-08-23 18:55:23 +0800

    1

---
+++ MyAlgorithm_JAVA/src/全排列.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class 全排列 {
+
+	List<List<Integer>> permute(int[] nums){
+		//记录【路径】
+		LinkedList<Integer> trackIntegers = new LinkedList<Integer>();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<List<Integer>> res = new LinkedList<List<Integer>>();
+		
+		/*主函数,输入一组不重复的数字,返回它们的全排列*/
+	}
+	
+	
+
+}
---
+++ MyAlgorithm_JAVA/src/数组子集.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;;
+
+public class 数组子集 {
+
+	public static List<List<Integer>> subsets(int[] nums) {
+		//base case返回一个空集
+		if (nums.length == 0) {
+			return null;
+		}
+		List<List<Integer>> outputList = new ArrayList<List<Integer>>();
+		
+		System.out.println();
+		return null;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
