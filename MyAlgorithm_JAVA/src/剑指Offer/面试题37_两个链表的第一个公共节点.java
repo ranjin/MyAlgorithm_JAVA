649950c114a28dbcf8b5d965a6a858895e090a4b
Charles Ran <jin7618@qq.com>
2020-09-01 09:59:30 +0800

    1

--- MyAlgorithm_JAVA/src/剑指Offer/面试题37_两个链表的第一个公共节点.java
+++ MyAlgorithm_JAVA/src/剑指Offer/面试题37_两个链表的第一个公共节点.java
@@ -2,24 +2,26 @@ package 剑指Offer;
 
 public class 面试题37_两个链表的第一个公共节点 {
 
-	
 	public class ListNode{
 		int val;
 		ListNode next;
-	} 
+	}
 	
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	public ListNode getIntersectioNode(ListNode headA, ListNode headB) {
 		if (headA == null || headB == null) {
 			return null;
 		}
-		ListNode node1 = headA;
-		ListNode node2 = headB;
-		while (node1 != node2) {
-			node1 = node1.next == null ? headB : node1.next;
-			node2 = node2.next == null ? headA : node2.next;
+		ListNode aNode = headA;
+		ListNode bNode = headB;
+		
+		if (aNode != bNode) {
+			aNode = aNode.next == null ? headB : aNode.next;
+			bNode = bNode.next == null ? headA : aNode.next;
 		}
-		return node1;
+		return aNode;
 	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
