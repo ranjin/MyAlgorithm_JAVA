7cedbd43ac987a73ef0f8c757e6a35a508971e8b
ranjin <ranjin@meituan.com>
2023-02-20 18:51:00 +0800

    删除数组重复元素

---
+++ MyAlgorithm_JAVA/src/LC_初级算法/LeeCode89_删除有序数组中的重复项2.java
@@ -0,0 +1,32 @@
+package LC_初级算法;
+
+/**
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，
+ * 使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+ * 输入：nums = [1,1,1,2,2,3]
+ * 输出：5, nums = [1,1,2,2,3]
+ *
+ */
+
+
+//		  slow    		  		  fast 
+//[1,		1,		1,		1,		2,		2,		2,		3]
+public class LeeCode89_删除有序数组中的重复项2 {
+
+    public int removeDuplicates(int[] nums) {
+    	int n = nums.length;
+    	if (n <= 2) {
+			return n;
+		}
+    	int slow = 2, fast = 2;
+    	while (fast < n) {
+			// 如果大于1
+			if (nums[slow - 2] != nums[fast]) {	// 表示有超过2个一样的元素了
+				nums[slow] = nums[fast];
+				slow++;
+			}
+			fast++;
+		}
+    	return slow;
+    }
+}
--- MyAlgorithm_JAVA/src/LC_初级算法/leecode_26_删除有序数组中的重复项.java
+++ MyAlgorithm_JAVA/src/LC_初级算法/leecode_26_删除有序数组中的重复项.java
@@ -7,31 +7,30 @@ package LC_初级算法;
  思路：快慢指针，将不相同的数组元素都移到数组左侧
  */
 
-// 【1,3,5,5,6】
+// 		 		slow  		  fast
+// 【1,    3,     5,    5,      6】
+
 public class leecode_26_删除有序数组中的重复项 {
-	
-    public static int removeDuplicates(int[] nums) {
-    	int length = nums.length;
-    	
-    	int slow = 0, fast = 1;
-    	
-    	// fast < length
-    	while (fast < length) {
-        	if (nums[slow] == nums[fast]) {
-    			fast++;
-    		}else {
-    			slow++;
-    			nums[slow] = nums[fast];
-    			fast++;
-    		}
+	public static int removeDuplicates(int[] nums) {
+		int n = nums.length;
+		if (n == 0) {
+			return 0;
 		}
-    	return slow+1;
-    }
+		int fast = 1, slow = 1;	// 删除重复元素之后至少剩一个元素
+		while (fast < n) {
+			if (nums[fast] != nums[fast - 1]) {
+				nums[slow] = nums[fast];
+				slow++;
+			}
+			fast++;
+		}
+		return slow;	// 从num[0]到num[slow-1]的每个元素都不相同
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = new int[] {0, 0, 1, 1, 3, 5, 5, 6};
 		int count = removeDuplicates(nums);
 		System.out.print(count);
 	}
-
 }
