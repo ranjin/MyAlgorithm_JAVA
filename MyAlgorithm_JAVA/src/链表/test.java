a12bacbd8dcf9c146b641938606ca45e3e72ce7d
ranjin <jin7618@qq.com>
2023-04-18 09:43:53 +0800

    test代码

--- MyAlgorithm_JAVA/src/链表/test.java
+++ MyAlgorithm_JAVA/src/链表/test.java
@@ -6,10 +6,11 @@ public class ListNode {
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
         ListNode dummy = new ListNode(-1);
         ListNode p = dummy;
-        ListNode l1 = list1, l2 = list2;
+        ListNode l1 = list1;
+        ListNode l2 = list2;
+
         while (l1 != null && l2 != null) {
             if (l1.val > l2.val) {
                 p.next = l2;
@@ -18,8 +19,10 @@ public class Solution {
                 p.next = l1;
                 l1 = l1.next;
             }
+            // p不断前进
             p = p.next;
         }
+
         if (l1 == null) {
             p.next = l2;
         }
