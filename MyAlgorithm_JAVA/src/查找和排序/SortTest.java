4779538352def5a80971d0c96326860f96f7b9ba
ranjin <jin7618@qq.com>
2024-10-28 11:45:19 +0800

    sdfsdf

--- MyAlgorithm_JAVA/src/查找和排序/SortTest.java
+++ MyAlgorithm_JAVA/src/查找和排序/SortTest.java
@@ -1,7 +1,12 @@
 package 查找和排序;
 
-import java.util.Iterator;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import 链表.LeeCode21_合并两个有序的链表.ListNode;
+
 
 public class SortTest {
 
@@ -13,32 +18,49 @@ public class SortTest {
 			this.val = val;
 		}
 	}
-
-    public boolean isValid(String s) {
-    	Stack<Character> stack = new Stack<Character>();
-    	char[] ss = s.toCharArray();
+	
+	/**
+	 * 合并两个有序链表
+	 * 1 -> 2 -> 3
+	 * 2 -> 3 -> 4
+	 */
+	
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    	if (list1 == null) {
+			return list2;
+		}
     	
-    	for (char c : ss) {
-			if (c == '{' || c == '(' || c == '[') {
-				stack.push(c);
+    	if (list2 == null) {
+			return list1;
+		}
+    	
+    	ListNode dummy = new ListNode(-1);
+    	
+    	ListNode p = dummy;
+    	ListNode p1 = list1, p2 = list2;
+    	
+    	while (p1 != null && p2 != null) {
+			if (p1.val > p2.val) {
+				p.next = p2;
+				p2 = p2.next;
 			} else {
-				if (!stack.isEmpty() && leftOfChar(c) == stack.peek()) {
-					stack.pop();
-				} else {
-					return false;
-				}
+				p.next = p1;
+				p1 = p1.next;		
 			}
-
+			p = p.next;
 		}
-    	return stack.isEmpty();
+    	if (p1 == null) {
+			p.next = p2;
+		} 
+    	
+    	if (p2 == null) {
+			p.next = p1;
+		}
+    	
+    	
+    	return dummy.next;
+    	
     }
-    
-    public static char leftOfChar(char c) {
-		if (c == '}') {
-			return '{';
-		} else if (c == ']') {
-			return '[';
-		}
-		return '(';
-	}
+
+
 }
--- MyAlgorithm_JAVA/src/查找和排序/leecode215_数组中的第K个最大元素.java
+++ MyAlgorithm_JAVA/src/查找和排序/leecode215_数组中的第K个最大元素.java
@@ -4,47 +4,34 @@ import java.util.PriorityQueue;
 
 public class leecode215_数组中的第K个最大元素 {
 
+	// 示例: 3 - 1 - 4 - 5 - 2 - 9
+	// 解法1: 优先级队列
     public int findKthLargest(int[] nums, int k) {
-    	
     	/**
     	 * 优先级队列
-    	 * 二插堆插入合删除的时间复杂度和堆中的元素个数有关，在这里我们堆的大小不会超过k。
-    	 * 总的时间复杂度为：O(N*LogK) n为数组元素总数， 所以最终的时间复杂度就是O(K)
-    	 * 
-    	 * 
-    	 * 
+    	 * 第K个最大元素，使用小顶堆，堆顶元素最小。
+    	 * 只保留K个元素，那么堆顶的就是最小的
+    	 * 总的时间复杂度为：O(N*LogK) n为数组元素总数， 所以最终的时间复杂度就是O(K) 
     	 */
     	PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-    	
     	for (int i = 0; i < nums.length; i++) {
 			queue.offer(nums[i]);
-			
 			if (queue.size() > k) {
 				queue.poll();
 			}
 		}
-    	
     	return queue.peek();
     }
-    
-    
-    
-    /// 快速排序
+
     /**
-     * 快速排序
+     * 快速选择算法
+     * 第K个最大元素，也就是第n-k个元素
      * 一直在进行partition操作：即N + N / 2 + N / 4 = O(N)
      * 时间复杂度为：O(N), 最坏情况是O(N^2)
-     * @param nums
-     * @param k
-     * @return
      */
     public int findKthLargest2(int[] nums, int k) {
     	k = nums.length - k;
     	int lo = 0, hi = nums.length - 1;
-    	
-    	// lo .....  partition .. .k . . .hi
-    	// 
-    	
     	while (lo < hi) {
         	int partition = findPartition(nums, lo, hi);
         	if (partition < k) {
